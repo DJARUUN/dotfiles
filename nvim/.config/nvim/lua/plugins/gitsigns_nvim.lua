166fece775f6b1c9bad19bd375819cdca91a8f5d
DJARUUN <caargajac@gmail.com>
2024-06-24 17:07:57 +0200

    Added some padding to gitsigns

--- nvim/.config/nvim/lua/plugins/gitsigns_nvim.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns_nvim.lua
@@ -2,11 +2,11 @@ return {
 	"lewis6991/gitsigns.nvim",
 	opts = {
 		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
+			add = { text = " +" },
+			change = { text = " ~" },
+			delete = { text = " _" },
+			topdelete = { text = " ‾" },
+			changedelete = { text = " ~" },
 		},
 	},
 }
