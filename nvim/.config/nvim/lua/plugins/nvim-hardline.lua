f76e969a68ec7cb635b7c01f5bd525364b2b747a
DJARUUN <caargajac@gmail.com>
2024-07-08 21:24:46 +0200

    Removed redundant nvim_hardline config

--- nvim/.config/nvim/lua/plugins/nvim-hardline.lua
+++ nvim/.config/nvim/lua/plugins/nvim-hardline.lua
@@ -3,8 +3,6 @@ return {
 	config = function()
 		require("hardline").setup({
 			bufferline = true,
-			bufferline_settings = {},
-			theme = "default",
 			sections = {
 				"[",
 				{ class = "mode", item = require("hardline.parts.mode").get_item },
@@ -20,20 +18,6 @@ return {
 				"|",
 				{ class = "mode", item = require("hardline.parts.line").get_item },
 				">",
-			},
-
-			custom_theme = {
-				text = { gui = "NONE" },
-				normal = { gui = "NONE" },
-				insert = { gui = "NONE" },
-				replace = { gui = "NONE" },
-				inactive_comment = { gui = "NONE" },
-				inactive_cursor = { gui = "NONE" },
-				inactive_menu = { gui = "NONE" },
-				visual = { gui = "NONE" },
-				command = { gui = "NONE" },
-				alt_text = { gui = "NONE" },
-				warning = { gui = "NONE" },
 			},
 		})
 	end,
