7347714c5aff35f167d53115bd593dd63911f31e
DJARUUN <caargajac@gmail.com>
2024-09-08 23:35:51 +0200

    Add support for system clipboard on wsl

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,16 @@
+vim.g.clipboard = {
+	name = "WslClipboard",
+	copy = {
+		["+"] = "clip.exe",
+		["*"] = "clip.exe",
+	},
+	paste = {
+		["+"] = [[powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]],
+		["*"] = [[powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]],
+	},
+	cache_enabled = 0,
+}
+
 require("config.vim_options")
 require("config.lazy")
 require("config.cmds")
