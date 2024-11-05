71afaa25d63e185c5f5c0bcf6762b2d5189ecc38
DJARUUN <caargajac@gmail.com>
2024-11-05 07:57:32 +0100

    Fix formatting

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -12,33 +12,52 @@ return {
 			desc = "Format buffer",
 		},
 	},
-	opts = {
-		notify_on_error = false,
-		format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
-		stop_after_first = true,
-		formatters_by_ft = {
-			lua = { "stylua" },
-			ocaml = { "ocamlformat" },
-			python = { "black" },
-			fennel = { "fnlfmt" },
-			php = { "pretty-php" },
-		},
-		formatters = {
-			ocamlformat = {
-				env = {
-					OCAMLFORMAT = "profile=default,type-decl=sparse,module-item-spacing=preserve,break-infix=fit-or-vertical",
+	config = function()
+		require("conform").setup({
+			notify_on_error = false,
+			format_on_save = function(bufnr)
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { timeout_ms = 500, lsp_format = "fallback" }
+			end,
+			stop_after_first = true,
+			formatters_by_ft = {
+				lua = { "stylua" },
+				ocaml = { "ocamlformat" },
+				python = { "black" },
+				fennel = { "fnlfmt" },
+				php = { "pretty-php" },
+			},
+			formatters = {
+				ocamlformat = {
+					env = {
+						OCAMLFORMAT = "profile=default,type-decl=sparse,module-item-spacing=preserve,break-infix=fit-or-vertical",
+					},
 				},
 			},
-			-- php = {
-			-- 	command = "php-cs-fixer",
-			-- 	args = {
-			-- 		"fix",
-			-- 		"$FILENAME",
-			-- 	},
-			-- 	stdin = false,
-			-- },
-		},
-	},
+		})
+
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
+	end,
+
 	init = function()
 		vim.o.formatexpr = [[v:lua.require("conform").formatexpr()]]
 	end,
