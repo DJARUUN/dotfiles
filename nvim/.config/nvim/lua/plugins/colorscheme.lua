347c6daf23fc49d582f4f342a7c3779004ee1c4e
DJARUUN <caargajac@gmail.com>
2024-11-14 08:55:47 +0100

    Fix ccolorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,31 +1,30 @@
 return {
-	{
-		"F4LCn/oxocharcoal.nvim",
-		config = function()
-			local function update_hl(group, tbl)
-				local old_hl = vim.api.nvim_get_hl_by_name(group, true)
-				local new_hl = vim.tbl_extend("force", old_hl, tbl)
-				vim.api.nvim_set_hl(0, group, new_hl)
-			end
-
-			update_hl("@function.call", { italic = true })
-			update_hl("@function.method.call", { italic = true })
-			update_hl("Function", { italic = true })
-			update_hl("@type", { italic = true })
-			update_hl("@type.builtin", { italic = true })
-			update_hl("StorageClass", { italic = true })
-			update_hl("@constructor.lua", { italic = false })
-			update_hl("@label", { italic = true })
-			update_hl("@keyword", { italic = true })
-			update_hl("Keyword", { italic = true })
-			update_hl("@keyword.import", { italic = true })
-
-			vim.cmd.colorscheme("oxocharcoal")
-		end,
-	},
+	-- {
+	-- 	"F4LCn/oxocharcoal.nvim",
+	-- 	config = function()
+	-- 		local function update_hl(group, tbl)
+	-- 			local old_hl = vim.api.nvim_get_hl_by_name(group, true)
+	-- 			local new_hl = vim.tbl_extend("force", old_hl, tbl)
+	-- 			vim.api.nvim_set_hl(0, group, new_hl)
+	-- 		end
+	--
+	-- 		update_hl("@function.call", { italic = true })
+	-- 		update_hl("@function.method.call", { italic = true })
+	-- 		update_hl("Function", { italic = true })
+	-- 		update_hl("@type", { italic = true })
+	-- 		update_hl("@type.builtin", { italic = true })
+	-- 		update_hl("StorageClass", { italic = true })
+	-- 		update_hl("@constructor.lua", { italic = false })
+	-- 		update_hl("@label", { italic = true })
+	-- 		update_hl("@keyword", { italic = true })
+	-- 		update_hl("Keyword", { italic = true })
+	-- 		update_hl("@keyword.import", { italic = true })
+	--
+	-- 		vim.cmd.colorscheme("oxocharcoal")
+	-- 	end,
+	-- },
 	{
 		"djaruun/vague.nvim",
-		enabled = false,
 		config = function()
 			require("vague").setup({
 				style = {
