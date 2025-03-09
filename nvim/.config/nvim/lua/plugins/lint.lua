0f79867bd348628624c9438156b2dfcbe3af8249
DJARUUN <caargajac@gmail.com>
2025-03-09 17:46:06 +0100

    Yes box

--- nvim/.config/nvim/lua/plugins/lint.lua
+++ nvim/.config/nvim/lua/plugins/lint.lua
@@ -4,7 +4,9 @@ return {
 	config = function()
 		local lint = require("lint")
 
-		lint.linters_by_ft = {}
+		lint.linters_by_ft = {
+			swift = { "swiftlint" },
+		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
